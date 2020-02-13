8a8d28a2f6ee8d367cb708a590c0882f6ca6e1e8
RamonaEJSpence <ramonaejspence@gmail.com>
2020-02-12 19:15:07 -0500

    Right now, my while loop is only toggling between player1 and player2 for some reason. I haven't quite figured it out yet.

--- a.py
+++ a.py
@@ -4,17 +4,42 @@ deck = list(range(1, 14)) * 4
 # print(deck)
 
 random.shuffle(deck)
-print(deck)
+# print(deck)
 
 player_one = deck[0:53:2]
 print(player_one)
 
-player_two = deck[0:53:2]
+player_two = deck[1:53:2]
 print(player_two)
 
 
+while len(player_one) > 0 and len(player_two) > 0:
+    card_one = player_one.pop()
+    print(card_one)
+    card_two = player_two.pop()
+    print(card_two)
+    if card_one > card_two:
+        print('Player1 Takes the Cards')
+        player_one.insert(0, card_one)
+        player_one.insert(0, card_two)
+        print(len(player_one))
+    elif card_one < card_two:
+        print('Player2 Takes the Cards')
+        player_two.insert(0, card_one)
+        player_two.insert(0, card_two)
+        print(len(player_two))
+    elif card_one == card_two:
+        print('Tie! Draw Three!')
+        card_one = player_one.pop()
+        card_two = player_two.pop()
+        print(card_one)
+        print(card_two)
+else:
+    if len(player_one) == 0 or len(player_two) == 0:
+        print('game over')
 #
-# player_one_deck = (random.choice(deck))
-# print(player_one_deck)
-
-# player_one_deck = random.randint(range(0, 53))
+# else:
+#     if len(player_one) == 0:
+#         print('Player 1 Wins!')
+#     elif len(player_two) == 0:
+#         print('Player 2 Wins!')
