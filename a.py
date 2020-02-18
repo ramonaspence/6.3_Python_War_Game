fe092882ec6b804841a6805514b4acee3bf26473
RamonaEJSpence <ramonaejspence@gmail.com>
2020-02-17 20:12:40 -0500

    I randomized the card that gets popped from the players hand, in the range of (1,5) and that solved my issue with the game just alternating who wins. I then wrote out an elif that slices 3 cards from the player lists, compares the 3rd and goes on.

--- a.py
+++ a.py
@@ -1,45 +1,57 @@
 import random
 
-deck = list(range(1, 14)) * 4
-# print(deck)
+def createdeck():
+    deck = list(range(1, 14)) * 4
+    random.shuffle(deck)
 
-random.shuffle(deck)
-# print(deck)
+    return deck
 
-player_one = deck[0:53:2]
-print(player_one)
+def main():
+    deck = createdeck()
 
-player_two = deck[1:53:2]
-print(player_two)
+    player_one = deck[0:52:2]
+    player_two = deck[1:52:2]
 
-
-while len(player_one) > 0 and len(player_two) > 0:
-    card_one = player_one.pop()
-    print(card_one)
-    card_two = player_two.pop()
-    print(card_two)
-    if card_one > card_two:
-        print('Player1 Takes the Cards')
-        player_one.insert(0, card_one)
-        player_one.insert(0, card_two)
-        print(len(player_one))
-    elif card_one < card_two:
-        print('Player2 Takes the Cards')
-        player_two.insert(0, card_one)
-        player_two.insert(0, card_two)
-        print(len(player_two))
-    elif card_one == card_two:
-        print('Tie! Draw Three!')
-        card_one = player_one.pop()
-        card_two = player_two.pop()
+    playeronelen = len(player_one)
+    playertwolen = len(player_two)
+    while (playeronelen > 0) or (playertwolen > 0):
+        # try:
+        random_card = random.randint(1, 5)
+        card_one = player_one.pop(random_card)
         print(card_one)
+        card_two = player_two.pop(random_card)
         print(card_two)
-else:
-    if len(player_one) == 0 or len(player_two) == 0:
-        print('game over')
-#
-# else:
-#     if len(player_one) == 0:
-#         print('Player 1 Wins!')
-#     elif len(player_two) == 0:
-#         print('Player 2 Wins!')
+        dealt = [card_one, card_two]
+        if card_one > card_two:
+            # print(len(player_one),player_one)
+            player_one.insert(-1, card_one)
+            player_one.insert(-1, card_two)
+            print('Player1 Takes the Cards',len(player_one),player_one)
+
+        elif card_two > card_one:
+            # print(len(player_two), player_two)
+            player_two.insert(-1, card_one)
+            player_two.insert(-1, card_two)
+            print('Player2 Takes the Cards',len(player_two), player_two)
+
+        elif card_two == card_one:
+            hand_one = player_one[0:3]
+            hand_two = player_two[0:3]
+            if hand_one[2] > hand_two[2]:
+                hand = hand_one + hand_two
+                player_one.extend(hand)
+            else:
+                hand = hand_one + hand_two
+                player_two.extend(hand)
+
+        # except:
+        #     print("ran out of cards!")
+        #     break
+
+    if len(player_one) >= 52 or len(player_two) >= 52:
+        print('Player 2 Won!', len(player_two))
+    else:
+        print('Player 1 Won!', len(player_one))
+
+
+main()
